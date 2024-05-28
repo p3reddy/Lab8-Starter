830f2b3ab4375bb67efa6f64c1028abf25bdb7d0
p3reddy <p3reddy@ucsd.edu>
2024-05-28 15:05:19 -0700

    fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -88,7 +88,7 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   const recipes = localStorage.getItem('recipes');
-  if (recipes.length === null)
+  if (recipes === null)
     return await JSON.parse(recipes);
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
